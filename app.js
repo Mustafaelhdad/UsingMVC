34b418cd3194a58b3c7e1a5f8350e862c4d87826
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2021-01-10 06:46:56 +0200

    Started using handlebars templating

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const express = require('express')
 // installed using npm
 const bodyParser = require('body-parser')
 const app = express()
+// calling handlebars templating engine
+const expressHbs = require('express-handlebars')
 
+//using the engine of the handlebars
+app.engine('hbs', expressHbs)
 //telling that we use the pug templating
-app.set('view engine', 'pug')
+app.set('view engine', 'hbs')
+//telling that we use the handlebars templating
+// app.set('view engine', 'pug')
 //where to find those files for templating
 app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const { use } = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
