643c487623e0761e7c7a7f5b8998a763dd33706f
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2021-01-11 03:00:00 +0200

    Finished using file for data storage

--- models/product.js
+++ models/product.js
@@ -1,18 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 
+const p = path.join(__dirname, '..', 'data', 'products.json')
+
+const getProductFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([])
+    }
+    cb(JSON.parse(fileContent))
+  })
+}
+
 module.exports = class Product {
   constructor (t) {
     this.title = t
   }
 
   save() {
-    const p = path.join(__dirname, '..', 'data', 'products.json')
-    fs.readFile(p, (err, fileContent) => {
-      let products = []
-      if (!err) {
-        products = JSON.parse(fileContent)
-      }
+    getProductFromFile(products => {
       products.push(this)
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err)
@@ -21,12 +27,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(__dirname, '..', 'data', 'products.json')
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([])
-      }
-      cb(JSON.parse(fileContent))
-    })
+    getProductFromFile(cb)
   }
 }
